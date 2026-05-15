42d2d9012ab74ce83d5261d7ae6dddba4852b4a1
hJun-KR <h0ngjunp.park@gmail.com>
2026-05-15 14:10:38 +0900

    feat: 수정 adapter 사용으로 방식 변환, config 세팅에서 가져오도록 수정

--- Back/src/prisma/prisma.service.ts
+++ Back/src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, INestApplication, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL as string,
-      }),
+  constructor(config: ConfigService) {
+    const pool = new Pool({
+      connectionString: config.get<string>('DATABASE_URL'),
     });
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
   }
 
   async onModuleInit() {
