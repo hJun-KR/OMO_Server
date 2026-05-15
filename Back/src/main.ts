3df912ae8176e301ba9fc1e86ef8451738d5d0dd
hJun-KR <h0ngjunp.park@gmail.com>
2026-05-15 14:40:20 +0900

    feat: ValidationPipe 설정 추가

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -2,10 +2,23 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+    }),
+  );
 
   prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT ?? 3000);
