3df3fcb54f6d59c2b73d5b82350c73c650f38605
hJun-KR <h0ngjunp.park@gmail.com>
2026-06-12 10:14:33 +0900

    feat: PostModule, FashionModule 앱 모듈에 등록

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './common/auth/auth.module';
+import { PostModule } from './modules/post/post.module';
+import { FashionModule } from './modules/fashion/fashion.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    PrismaModule,
+    AuthModule,
+    PostModule,
+    FashionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
